437fc8be90d08dc19c12befa755ca10667334c17
Nicole Serras <citysceneapp@gmail.com>
2018-12-16 18:50:50 -0500

    Additional dependencies and contact info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
-    render() {
-        return (
-            <div className='App'>
-                <header className='App-header'>
-                    <p>Something brewing Something</p>
-                    <a
-                        className='App-link'
-                        href='https://reactjs.org'
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        Learn About it
-                    </a>
-                </header>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <p>CityScene App Brewing</p>
+          <a href='mailto:citysceneapp@gmail.com'>
+                        Email us at citysceneapp@gmail.com
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
